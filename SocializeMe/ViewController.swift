007125ddfbb668098b83fd63625fa63409223a17
Abhiram Kadiyala <abhiramkadiyala@Abhirams-MacBook-Pro.local>
2019-02-28 17:19:26 -0600

    Added login view and segues for profile view and create profile

--- SocializeMe/ViewController.swift
+++ SocializeMe/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButtonPressed: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "ProfileViewSegue", sender: self)
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "FormViewSegue", sender: self)
+    }
 }
 
